1b0509ff3371ae02e24bce530c6321492c1e2183
VisakhJB <visakhjb1234@gmail.com>
2023-06-25 16:02:40 +0530

    first commit

---
+++ cypress/integration/Features/LoginFeatureFiles/Login.feature
@@ -0,0 +1,6 @@
+Feature: Orange HRM Login Feature
+Scenario: User should be able to login with valid credentials
+Given User is on the login page
+When user provides username
+And user provides passoword
+Then user click on login button
---
+++ cypress/integration/StedDefinitions/Login/login.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+
+import { LoginPageActions } from "../../../../PageObjects/PageActions/LoginPageActions";
+import {Given,When,Then,And} from "cypress-cucumber-preprocessor/steps";
+
+const loginPageActions = new LoginPageActions();
+
+    Given('User is on the login page',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    })
+
+    When('user provides username',()=>{
+        loginPageActions.username('admin');
+    })
+
+    And('user provides passoword',()=>{
+        loginPageActions.password('admin123');
+    })
+
+    Then('user click on login button',()=>{
+        loginPageActions.login();   
+    })
